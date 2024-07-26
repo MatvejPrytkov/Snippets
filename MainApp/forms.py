83504c5bbbe1c7dc4170251f1093563f6405105d
Matvej Prytkov <matvej.prytkov.02@bk.ru>
2024-07-26 11:52:13 +0500

    Public

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -6,8 +6,8 @@ class SnippetForm(ModelForm):
     class Meta:
         model = Snippet
         # Описываем поля, которые будем заполнять в форме
-        fields = ['name', 'lang', 'code']
-        labels = {'name': '', 'lang': '', 'code': ''}
+        fields = ['is_public','name', 'lang', 'code']
+        labels = {'is_public':'','name': '', 'lang': '', 'code': ''}
         widgets = {
           'name': TextInput(
               attrs={"class":"form-control", 'style': 'max-width: 350px;', 'placeholder': 'Название сниппета'}
